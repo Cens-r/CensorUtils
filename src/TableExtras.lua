664d3d85cb144884b075c73e38f835ae3371f614
Censor <22554514+Cens-r@users.noreply.github.com>
2023-11-29 14:57:05 -0500

    Added TableExtras.get()

--- src/TableExtras.lua
+++ src/TableExtras.lua
@@ -1,7 +1,7 @@
 local TableExtras = {}
 
 -- Counts the number of elements in a table
-function TableExtras.count(tbl)
+function TableExtras.count(tbl: {[any]: any})
     local count = 0
     for _, _ in tbl do
         count += 1
@@ -10,18 +10,28 @@ function TableExtras.count(tbl)
 end
 
 -- Swaps two values in a table
-function TableExtras.swap(tbl, i1, i2)
+function TableExtras.swap(tbl: {[any]: any}, i1: any, i2: any)
     local tmp = tbl[i1]
     tbl[i1] = tbl[i2]
     tbl[i2] = tmp
 end
 
 -- Reverse an array's values
-function TableExtras.reverse(tbl)
+function TableExtras.reverse(tbl: {[any]: any})
     local size = #tbl
     for index = 1, size // 2 do
         TableExtras.swap(tbl, index, (size + 1) - index)
     end
+end
+
+-- Get a value from a table key, or initialize the key with a default value and return it
+function TableExtras.get(tbl: {[any]: any}, key: any, default: any)
+    local value = tbl[key]
+    if value == nil then
+        tbl[key] = default
+        value = default
+    end
+    return value
 end
 
 -- Source: https://create.roblox.com/docs/luau/tables#deep-clones
